4e67a2a564fdc2f0335dd1d49243fdcc9b1f8f70
Christoph Hagen <github@christophhagen.de>
2023-01-18 00:58:40 +0100

    Update iOS version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Clairvoyant",
-    platforms: [.macOS(.v12), .iOS(.v13)],
+    platforms: [.macOS(.v12), .iOS(.v14)],
     products: [
         .library(
             name: "Clairvoyant",
