78dc163c7d46694000f25a554e707a34441f8ec1
Christoph Hagen <github@christophhagen.de>
2023-01-29 14:40:56 +0100

    Add initializers to access token

--- Sources/Clairvoyant/Client/MetricAccessToken.swift
+++ Sources/Clairvoyant/Client/MetricAccessToken.swift
@@ -3,4 +3,19 @@ import Foundation
 public struct MetricAccessToken: MetricAccessTokenProvider {
 
     public let accessToken: Data
+
+    public init(accessToken: Data) {
+        self.accessToken = accessToken
+    }
+
+    public init?(base64String: String) {
+        guard let accessToken = Data(base64Encoded: base64String) else {
+            return nil
+        }
+        self.accessToken = accessToken
+    }
+
+    public init(string: String) {
+        self.accessToken = string.data(using: .utf8)!
+    }
 }
