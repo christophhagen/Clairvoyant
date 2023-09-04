caec73d43e8ad58994f377066b66bdfed3af009a
Christoph Hagen <github@christophhagen.de>
2023-09-04 09:40:09 +0200

    Expose metric hashing function

--- Sources/Clairvoyant/Metric/MetricId.swift
+++ Sources/Clairvoyant/Metric/MetricId.swift
@@ -6,7 +6,7 @@ public typealias MetricIdHash = String
 
 extension MetricId {
 
-    func hashed() -> MetricIdHash {
+    public func hashed() -> MetricIdHash {
         SHA256.hash(data: data(using: .utf8)!).prefix(16).hex
     }
 }
