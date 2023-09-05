5ba05cabe44237ad971901a2761ed183145e0ae2
Christoph Hagen <github@christophhagen.de>
2023-09-05 22:14:10 +0200

    Create server route prefix enum

--- Sources/Clairvoyant/ServerRoute.swift
+++ Sources/Clairvoyant/ServerRoute.swift
@@ -11,16 +11,57 @@ public enum ServerRoute {
 
     public var rawValue: String {
         switch self {
-        case .getMetricList: return "list"
-        case .lastValue(let hash): return "last/\(hash)"
-        case .allLastValues: return "last/all"
-        case .extendedInfoList: return "list/extended"
-        case .metricHistory(let hash): return "history/\(hash)"
-        case .pushValueToMetric(let hash): return "push/\(hash)"
+        case .getMetricList: return Prefix.getMetricList.rawValue
+        case .lastValue(let hash): return Prefix.lastValue.appending(hash: hash)
+        case .allLastValues: return Prefix.allLastValues.rawValue
+        case .extendedInfoList: return Prefix.extendedInfoList.rawValue
+        case .metricHistory(let hash): return Prefix.metricHistory.appending(hash: hash)
+        case .pushValueToMetric(let hash): return Prefix.pushValueToMetric.appending(hash: hash)
         }
     }
 
     public static var headerAccessToken = "token"
+
+    public var prefix: Prefix {
+        switch self {
+        case .getMetricList:
+            return .getMetricList
+        case .lastValue:
+            return .lastValue
+        case .allLastValues:
+            return .allLastValues
+        case .extendedInfoList:
+            return .extendedInfoList
+        case .metricHistory:
+            return .metricHistory
+        case .pushValueToMetric:
+            return .pushValueToMetric
+        }
+    }
+
+    public enum Prefix: String {
+        case getMetricList = "list"
+        case lastValue = "last"
+        case allLastValues = "last/all"
+        case extendedInfoList = "list/extended"
+        case metricHistory = "history"
+        case pushValueToMetric = "push"
+
+        func with(hash: MetricIdHash) -> ServerRoute {
+            switch self {
+            case .getMetricList: return .getMetricList
+            case .lastValue: return .lastValue(hash)
+            case .allLastValues: return .allLastValues
+            case .extendedInfoList: return .extendedInfoList
+            case .metricHistory: return .metricHistory(hash)
+            case .pushValueToMetric: return .pushValueToMetric(hash)
+            }
+        }
+
+        func appending(hash: MetricIdHash) -> String {
+            return rawValue + "/" + hash
+        }
+    }
 }
 
 extension ServerRoute: Equatable {
